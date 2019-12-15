c89111d8c55b5adb5fd2c5d19deb9f42a11e83b5
Marcelo Pedras <marcelo.braulio.si@gmail.com>
2019-12-15 20:45:57 -0300

    Onebit Messenger. Clone do Facebook Messenger. Tutorial do onebitcode.com

---
+++ app/javascript/packs/application.js
@@ -0,0 +1,35 @@
+// This file is automatically compiled by Webpack, along with any other files
+// present in this directory. You're encouraged to place your actual application logic in
+// a relevant structure within app/javascript and only use these pack files to reference
+// that code so it'll be compiled.
+
+require("@rails/ujs").start()
+require("turbolinks").start()
+require("@rails/activestorage").start()
+require("channels")
+
+ 
+import bulmaQuickview from 'bulma-quickview/src/js'
+ 
+document.addEventListener('turbolinks:load', function() {
+  let quickviews = bulmaQuickview.attach()
+
+  const fileInput = document.querySelectorAll('input[type=file].file-input')
+  
+  fileInput.forEach(input => {
+    input.onchange = () => {
+      if (input.files.length > 0) {
+        const fileName = input.parentNode.querySelector(".file-name")
+        fileName.textContent = input.files[0].name
+      }
+    }
+  })
+})
+
+if (navigator.serviceWorker) {
+  navigator.serviceWorker.register('/service-worker.js', { scope: './' })
+    .then(function(reg) {
+      console.log('[Companion]', 'Service worker registered!');
+      console.log(reg);
+    });
+}
